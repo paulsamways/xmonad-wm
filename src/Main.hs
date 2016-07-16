94b22aecb3695cb724fe9d7f9d89a504219af8f7
Paul Samways <paul@paulsamways.com>
2016-07-16 17:44:56 +1000

    Changed window movement key bindings

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,32 @@
 module Main where
 
+import qualified Data.Map as M
 import XMonad
+import qualified XMonad.StackSet as W
+import XMonad.Hooks.Place
 import XMonad.Prompt
 import XMonad.Prompt.RunOrRaise
-import qualified Data.Map as M
+import XMonad.Hooks.DynamicLog
+
 
 main :: IO ()
-main = xmonad def { modMask  = mod4Mask
-                  , terminal = "urxvt"  
-                  , focusedBorderColor  = "#7cafc2"
-                  , normalBorderColor   = "#181818"
-                  , focusFollowsMouse   = True
-                  , keys     = keybindings <+> keys def
-                  }
+main = xmonad myConfig
 
-keybindings conf@(XConfig { XMonad.modMask = modm }) = M.fromList
-  [ ((modm, xK_F12), runOrRaisePrompt $ def { position = Top } )
-  ]
+myConfig = def { modMask             = mod4Mask
+               , terminal            = "urxvt"
+               , focusedBorderColor  = "#7cafc2"
+               , normalBorderColor   = "#181818"
+               , focusFollowsMouse   = True
+               , manageHook          = placeHook (inBounds (underMouse (0, 0))) <+> manageHook def
+               , keys                = keyBindings <+> keys def
+               }
+
+keyBindings conf@(XConfig {XMonad.modMask = modm}) =
+  M.fromList [ ((modm, xK_Up),    windows W.focusUp)              -- move between windows
+             , ((modm, xK_Down),  windows W.focusDown)
+             , ((modm, xK_Right), windows W.focusDown)
+             , ((modm, xK_Left),  windows W.focusMaster)
+
+             , ((modm .|. shiftMask, xK_Left), sendMessage Shrink) -- resize master/slave
+             , ((modm .|. shiftMask, xK_Right), sendMessage Expand)
+             ]
