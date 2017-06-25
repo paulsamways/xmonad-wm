29e5970a3588917e7b82ece3e747c1dffcdd0d51
Paul Samways <paul.samways@data61.csiro.au>
2017-06-25 19:22:09 +1000

    BSP

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,28 @@ module Main where
 import qualified Data.Map as M
 import XMonad
 import qualified XMonad.StackSet as W
-import XMonad.Hooks.Place
-import XMonad.Prompt
-import XMonad.Prompt.RunOrRaise
-import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageHelpers
+import Graphics.X11.ExtraTypes.XF86
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.Spacing
+import XMonad.Layout.ShowWName
+import XMonad.Layout.WindowSwitcherDecoration
+import XMonad.Layout.DraggingVisualizer
+import XMonad.Layout.Decoration
+import XMonad.Hooks.EwmhDesktops
 
+altMask = mod1Mask
 
 main :: IO ()
-main = xmonad myConfig
+main = xmonad $ ewmh myConfig
 
 myConfig = def { modMask             = mod4Mask
                , terminal            = "urxvt"
                , focusedBorderColor  = "#7cafc2"
                , normalBorderColor   = "#181818"
                , focusFollowsMouse   = True
-               , manageHook          = placeHook (inBounds (underMouse (0, 0))) <+> manageHook def
+               --, manageHook          = composeOne [ className =? "pocket" -?> doCenterFloat ] -- placeHook (inBounds (underMouse (0, 0))) <+> manageHook def
+               , layoutHook          = showWName $ smartSpacing 8 emptyBSP
                , keys                = keyBindings <+> keys def
                }
 
@@ -27,6 +34,37 @@ keyBindings conf@(XConfig {XMonad.modMask = modm}) =
              , ((modm, xK_Right), windows W.focusDown)
              , ((modm, xK_Left),  windows W.focusMaster)
 
-             , ((modm .|. shiftMask, xK_Left), sendMessage Shrink) -- resize master/slave
-             , ((modm .|. shiftMask, xK_Right), sendMessage Expand)
+             , ((modm, xK_q),     kill)
+
+             , ((0, xF86XK_AudioMute),          spawn "amixer sset Master toggle")
+             , ((0, xF86XK_AudioRaiseVolume),   spawn "amixer sset Master 5%+")
+             , ((0, xF86XK_AudioLowerVolume),   spawn "amixer sset Master 5%-")
+
+             , ((0, xF86XK_MonBrightnessUp),    spawn "xbacklight -inc 10")
+             , ((0, xF86XK_MonBrightnessDown),  spawn "xbacklight -dec 10")
+
+             , ((0, xK_Scroll_Lock),            spawn "lockscreen")
+             , ((0, xK_Print),                  spawn "gnome-screenshot")
+             , ((modm, xK_space),               spawn "rofi -show drun -matching fuzzy -levenshtein-sort")
+             , ((modm .|. shiftMask, xK_space), spawn "rofi -show window -matching fuzzy -levenshtein-sort")
+
+             , ((modm .|. controlMask, xK_Right),   sendMessage $ ExpandTowards R)
+             , ((modm .|. controlMask, xK_Left),    sendMessage $ ExpandTowards L)
+             , ((modm .|. controlMask, xK_Down),    sendMessage $ ExpandTowards D)
+             , ((modm .|. controlMask, xK_Up),      sendMessage $ ExpandTowards U)
+             
+             , ((modm .|. controlMask .|. shiftMask, xK_Right), sendMessage $ ShrinkFrom R)
+             , ((modm .|. controlMask .|. shiftMask, xK_Left), sendMessage $ ShrinkFrom L)
+             , ((modm .|. controlMask .|. shiftMask, xK_Down), sendMessage $ ShrinkFrom D)
+             , ((modm .|. controlMask .|. shiftMask, xK_Up), sendMessage $ ShrinkFrom U)
+             
+             , ((modm,                 xK_r), sendMessage Rotate)
+             , ((modm .|. controlMask, xK_r), sendMessage RotateL)
+             , ((modm .|. altMask,     xK_r), sendMessage RotateR)
+             
+             , ((modm,                 xK_s), sendMessage Swap)
+
+             , ((modm, xK_Tab), sendMessage FocusParent)
+             , ((modm .|. shiftMask, xK_Tab), sendMessage SelectNode)
+             , ((modm, xK_Escape), sendMessage MoveNode)
              ]
